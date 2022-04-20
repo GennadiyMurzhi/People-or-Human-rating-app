cd078a60a3e6280cc703dddec6decdc1d904bf3b
Gennadiy <gennadiy.murzhi@gmail.com>
2022-04-20 16:39:36 +0300

    database vendor was made

--- lib/domain/core/i_database_vendor.dart
+++ lib/domain/core/i_database_vendor.dart
@@ -3,5 +3,5 @@ import 'package:people_rating_app/domain/contacts/contacts.dart';
 abstract class IDatabaseVendor {
   Future<void> cacheContacts(Contacts contactsOfRegisteredUsers, Contacts contactsOfUnregisteredUsers);
   ///Keys must be of contactsOfRegisteredUsers and contactsUnRegisteredUsers
-  Future<Map<String, Contacts>> getCachedContacts(Contacts contactsOfRegisteredUsers, Contacts contactsOfUnregisteredUsers);
+  Future<Map<String, Contacts>> getCachedContacts();
 }
--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -5,26 +5,25 @@
 // **************************************************************************
 
 import 'package:dio/dio.dart' as _i7;
-import 'package:flutter/material.dart' as _i18;
+import 'package:flutter/material.dart' as _i17;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:internet_connection_checker/internet_connection_checker.dart'
-    as _i15;
-import 'package:sqflite/sqflite.dart' as _i12;
+    as _i14;
 
-import 'application/main_screen_cubit/main_screen_cubit.dart' as _i17;
+import 'application/main_screen_cubit/main_screen_cubit.dart' as _i16;
 import 'domain/contacts/i_contacts_local_data_source.dart' as _i3;
 import 'domain/contacts/i_contacts_remote_data_sorce.dart' as _i5;
 import 'domain/core/i_contacts_vendor.dart' as _i8;
 import 'domain/core/i_database_vendor.dart' as _i10;
-import 'domain/core/i_network_info.dart' as _i13;
+import 'domain/core/i_network_info.dart' as _i12;
 import 'infrastructure/contacts/contacts_local_data_source.dart' as _i4;
 import 'infrastructure/contacts/contacts_remote_data_source.dart' as _i6;
-import 'infrastructure/contacts/contacts_repository.dart' as _i16;
+import 'infrastructure/contacts/contacts_repository.dart' as _i15;
 import 'infrastructure/core/contacts_vendor.dart' as _i9;
 import 'infrastructure/core/database_vendor.dart' as _i11;
 import 'infrastructure/core/network_info.dart'
-    as _i14; // ignore_for_file: unnecessary_lambdas
+    as _i13; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -35,15 +34,14 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   gh.factory<_i5.IContactsRemoteDataSource>(
       () => _i6.ContactsRemoteDataSource(get<_i7.Dio>()));
   gh.factory<_i8.IContactsVendor>(() => _i9.ContactsVendor());
-  gh.factory<_i10.IDatabaseVendor>(
-      () => _i11.DatabaseVendor(get<_i12.Database>()));
-  gh.factory<_i13.INetworkInfo>(
-      () => _i14.NetworkInfo(get<_i15.InternetConnectionChecker>()));
-  gh.factory<_i16.ContactsRepository>(() => _i16.ContactsRepository(
+  gh.factory<_i10.IDatabaseVendor>(() => _i11.DatabaseVendor());
+  gh.factory<_i12.INetworkInfo>(
+      () => _i13.NetworkInfo(get<_i14.InternetConnectionChecker>()));
+  gh.factory<_i15.ContactsRepository>(() => _i15.ContactsRepository(
       get<_i9.ContactsVendor>(),
       get<_i5.IContactsRemoteDataSource>(),
       get<_i3.IContactsLocalDataSource>()));
-  gh.factory<_i17.MainScreenCubit>(() => _i17.MainScreenCubit(
-      get<_i18.PageController>(), get<_i16.ContactsRepository>()));
+  gh.factory<_i16.MainScreenCubit>(() => _i16.MainScreenCubit(
+      get<_i17.PageController>(), get<_i15.ContactsRepository>()));
   return get;
 }
